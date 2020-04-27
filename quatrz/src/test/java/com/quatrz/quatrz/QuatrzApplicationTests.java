b5326bb778c6200d2074601a7e7e151396a54d97
pengchenyi <1529565626@qq.com>
2020-04-27 18:33:11 +0800

    更新

---
+++ quatrz/src/test/java/com/quatrz/quatrz/QuatrzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quatrz.quatrz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuatrzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
